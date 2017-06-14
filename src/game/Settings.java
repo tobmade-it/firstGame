2d4dc9da90816c6a6ab95d48be7fe4b8bad18d73
tobmade-it <tobmade-it@users.noreply.github.com>
2017-06-14 19:51:45 +0200

    now added Settings class

---
+++ src/game/Settings.java
@@ -0,0 +1,14 @@
+package game;
+
+import java.awt.Color;
+
+public class Settings {
+	public static final int WINDOW_H = 1000;
+	public static final int WINDOW_W = 700;
+	public static final int GAME_H = 600;
+	public static final int GAME_W = 700;
+	public static final int FPS = 10;
+	
+	public static final Color BACKGROUND = new Color(56,56,56);
+	
+}
