f5dbb745b2233b7a8af4ebfc866c686866b44981
tobmade-it <tobmade-it@users.noreply.github.com>
2017-06-14 15:42:43 +0200

    GUI Draw Walls

--- src/gui/Render.java
+++ src/gui/Render.java
@@ -1,5 +1,25 @@
 package gui;
 
-public class Render {
+import java.awt.Color;
+import java.awt.EventQueue;
 
+import Generator.Generate;
+import game.Visible;
+
+public class Render {
+	public static void main(String args[]) {
+		int x = 70;
+		int y = 60;
+		
+		Visible[][] gameField = Generate.genDungeon(x, y);
+		System.out.println("FELD: " + gameField.length);
+		
+		EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Window ex = new Window(gameField);
+                ex.setVisible(true);
+            }
+        });
+	}
 }
